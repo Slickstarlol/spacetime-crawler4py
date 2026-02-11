55a0bc4b6af9bb276833ad85d125d5b2f3d738a0
agileflanker <azngo@uci.edu>
2026-02-10 18:51:44 -0800

    Fixed "outlook-cal" to "outlook-ical" and included .img in bad file extensions.

--- scraper.py
+++ scraper.py
@@ -16,12 +16,12 @@ MAX_QUERY_LEN = 5
 BAD_QUERY_KEYS = {"ns", "image", "tab", "tab_files", "tab_details", 
 				  "do", "diff", "sort", "filter", "view", "action",
 				  "auth", "from", "precision", "token", "filter",
-				  "search", "share", "outlook-cal", "ical", "keywords",
+				  "search", "share", "outlook-ical", "ical", "keywords",
 				  "redirect_to"}
 
 BAD_EXTENSIONS_REGEX = (r".*\.(7z|arff|avi|bib|bin|bmp|bz2|c|cnf|css|csv|"
 						r"dat|data|dll|dmg|doc|docx|eps|epub|exe|h|"
-						r"gif|gz|ico|iso|jar|jpe?g|jpg|js|json|lif|m4v|mid|"
+						r"gif|gz|ico|iso|img|jar|jpe?g|jpg|js|json|lif|m4v|mid|"
 						r"mkv|mov|mp2|mp3|mp4|mpeg|msi|mso|names|npy|"
 						r"odc|ogg|ogv|php|pdf|png|ppt|pptx|ppsx|pps|ps|psd|py|ram|"
 						r"rar|rm|rtf|sas|sha1|smil|swf|tar|tex|tgz|"
